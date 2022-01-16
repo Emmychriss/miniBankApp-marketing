9134f4ccaeac2d4f022116e4ad33c9bc123bd7ce
Emmanuel <emmychris477@gmail.com>
2022-01-16 20:23:01 +0100

    Open and close modal

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
