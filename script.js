ab4393f5616438debdb14f56456d202e904d022f
Emmanuel <emmychris477@gmail.com>
2022-01-27 20:01:51 +0100

    nav links smooth scrolls

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
+// element selections
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+// Modal window
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
@@ -31,11 +33,22 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
+// smooth scroll on 'learn more btn'
 btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({
     behavior: 'smooth',
+  });
+});
+
+// page navigation
+Array.from(document.querySelectorAll('.nav__link')).forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const id = this.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+    });
   });
 });
